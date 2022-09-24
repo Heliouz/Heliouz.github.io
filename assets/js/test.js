20e21e012ab8c4c630f3acaf3421cd1b1308d28c
Heliouz <heliosocana123@hotmail.com>
2022-09-24 11:59:50 -0500

    Update test.js

--- assets/js/test.js
+++ assets/js/test.js
@@ -3,27 +3,48 @@ Pts.namespace(this);
 var space = new CanvasSpace("#hello").setup({ bgcolor: "#fff" });
 var form = space.getForm();
 
-// animation
-space.add( (time, ftime) => {
+  var pairs = [];
 
-  // rectangle
-  var rect = Rectangle.fromCenter( space.center, space.size.$divide(2) );
-  var poly = Rectangle.corners( rect );
-  poly.shear2D( Num.cycle( time%5000/5000 ) - 0.5, space.center );
-  
-  // triangle
-  var tris = poly.segments( 2, 1, true );
-  tris.map( (t) => t.push( space.pointer ) );
-  
-  // circle
-  var circles = tris.map( (t) => Triangle.incircle( t ) );
-  
-  // drawing
-  form.fillOnly("#123").polygon( poly );
-  form.fill("#f03").circles( circles );
-  form.strokeOnly("#fff ", 3 ).polygons( tris );
-  form.fill("#123").point( space.pointer, 5 );
-  
-});
+  space.add({ 
 
-space.play().bindMouse();
+    start:( bound ) => {
+      let r = space.size.minValue().value/2;
+
+      // create 200 lines
+      for (let i=0; i<200; i++) {
+        let ln = new Group( Pt.make(2, r, true), Pt.make(2, -r, true) );
+        ln.moveBy( space.center ).rotate2D( i*Math.PI/200, space.center );
+        pairs.push(ln );
+      }
+    }, 
+
+    animate: (time, ftime) => {
+
+      for (let i=0, len=pairs.length; i<len; i++) {
+
+        // rotate each line by 0.1 degree and check collinearity with pointer
+        let ln = pairs[i];
+        ln.rotate2D( Const.one_degree/10, space.center );
+        let collinear = Line.collinear( ln[0], ln[1], space.pointer, 0.1);
+
+        if (collinear) {
+          form.stroke("#fff").line(ln);
+
+        } else {
+          // if not collinear, color the line based on whether the pointer is on left or right side
+          let side = Line.sideOfPt2D( ln, space.pointer );
+          form.stroke( (side<0) ? "rgba(255,255,0,.1)" : "rgba(0,255,255,.1)" ).line( ln );
+        }
+        form.fillOnly("rgba(255,255,255,0.8").points( ln, 0.5);
+      }
+
+      form.fillOnly("#f03").point( space.pointer, 3, "circle");
+      
+    }
+
+  });
+  
+  //// ----
+  
+
+  space.bindMouse().bindTouch().play();
