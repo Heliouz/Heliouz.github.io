7a788c4cc63492bd63083174b0cbda28b6e3010b
Heliouz <heliosocana123@hotmail.com>
2022-09-27 16:55:47 -0500

    Create glitch.js

---
+++ assets/js/glitch.js
@@ -0,0 +1,26 @@
+import GlitchedWriter, {
+  wait
+} from "https://cdn.skypack.dev/glitched-writer@2.0.29";
+
+// Glitched Writer npm module:
+// https://www.npmjs.com/package/glitched-writer
+
+const Writer = new GlitchedWriter('#glitch_this', { letterize: true }, logString);
+
+(async  ()=> {
+  await wait(1000);
+  await Writer.write("my old friend.");
+  
+  await wait(1200);
+  await Writer.write("This is only the beginning.");
+  
+  await wait(1500);
+  await Writer.write("Please, say something...");
+  
+  input.removeAttribute("disabled");
+})();
+
+function logString(string) {
+  logs.innerHTML += `<p>${string}</p>`;
+}
+);
