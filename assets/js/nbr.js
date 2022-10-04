27d40449364e38d1199044abbe6140bf729bb1cd
Heliouz <heliosocana123@hotmail.com>
2022-10-04 11:10:37 +0200

    Update nbr.js

--- assets/js/nbr.js
+++ assets/js/nbr.js
@@ -1,17 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
- function counter(id, start, end, duration) {
-  let obj = document.getElementById(id),
-   current = start,
-   range = end - start,
-   increment = end > start ? 1 : -1,
-   step = Math.abs(Math.floor(duration / range)),
-   timer = setInterval(() => {
-    current += increment;
-    obj.textContent = current;
-    if (current == end) {
-     clearInterval(timer);
-    }
-   }, step);
- }
- counter("nbr", 500, 910, 1500);
-});
+var speed = 1;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+  elt = document.getElementById(id);
+  endNbr = Number(document.getElementById(id).innerHTML);
+  incNbrRec(500, endNbr, elt);
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, endNbr, elt) {
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function() {//Delay a bit before calling the function again.
+      incNbrRec(i + 1, endNbr, elt);
+    }, speed);
+  }
+}
+
+/*Function called on button click*/
+function incNbr(){
+  incEltNbr("nbr");
+}
+
+incEltNbr("nbr"); /*Call this funtion with the ID-name for that element to increase the number within*/
